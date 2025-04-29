7fcff2fe5e97441c4270c80f77e278d9ddbf6051
Tom Oliver <tom@inmail.me>
2025-04-29 15:11:16 +0200

    Added missing docstrings

--- src/monsterizer_dataclasses/5e_dataclasses.py
+++ src/monsterizer_dataclasses/5e_dataclasses.py
@@ -1,7 +1,9 @@
+"""Dataclasses for creating monsters using DnD 5th Edition."""
+
 from dataclasses import dataclass, field
+
 from dataclass_exceptions import InvalidTypeError
 from dataclasses_json import dataclass_json
-
 
 # Base monster dataclass
 
@@ -9,9 +11,11 @@ from dataclasses_json import dataclass_json
 @dataclass(kw_only=True)
 class BaseMonster:
     """
-    Base dataclass for all monsters. All monster types derive
-    from this base. 
+    Base dataclass for all monsters.
+    
+    All monster types derive from this base. 
     """
+
     name: str
     size: str = field(default="medium")
     hitpoints: int = field(default=10)
@@ -30,6 +34,7 @@ class BaseMonster:
     bonus_actions: list = field(default=[])
 
     def __post_init__(self):
+        """Set up ability modifiers and base armor class."""
         self.str_mod = self.strength//2 - 10
         self.dex_mod = self.dexterity//2 - 10
         self.con_mod = self.constitution//2 - 10
@@ -43,24 +48,56 @@ class BaseMonster:
 @dataclass_json
 @dataclass(kw_only=True)
 class Aberration(BaseMonster):
+    """
+    Aberration.
+
+    Inherits from BaseMonster.
+
+    Could use gear.
+    """
+
     monster_type:str = "Aberration"
     gear:list | None = field(default=None, init=False)
 
 @dataclass_json
 @dataclass(kw_only=True)
 class Beast(BaseMonster):
+    """
+    Beast.
+
+    Inherits from BaseMonster.
+    
+    Does not use gear except for edge cases.
+
+    Uses natural armor unless specified.
+    """
+
     monster_type:str = "Beast"
     armor:str = "natural armor"
 
 @dataclass_json
 @dataclass(kw_only=True)
 class Celestial(BaseMonster):
+    """
+    Celestial.
+
+    Inherits from BaseMonster.
+
+    Could use gear.
+    """
+
     monster_type:str = "Celestial"
     gear: list | None = field(default=None, init=False)
 
 @dataclass_json
 @dataclass(kw_only=True)
 class Constructs(BaseMonster):
+    """
+    Construct.
+
+    Inherits from BaseMonster.
+    """
+
     monster_type:str = "Celestial"
     gear: list | None = field(default=None, init=False)
 
@@ -68,10 +105,17 @@ class Constructs(BaseMonster):
 
 @dataclass(kw_only=True)
 class Resistance:
+    """
+    Dataclass used to track any monster resistances.
+
+    Will be part of a list, even if there is just one resistance.
+    """
+
     type: str
     ac_modifier: float = field(default=0)
 
     def __post_init__(self):
+        """Validate that type is the correct format."""
         if not isinstance(self.type, str):
             raise InvalidTypeError("Not a valid type", self.type)
         if self.type in ["bludgeoning", "piercing", "slashing"]:
@@ -81,14 +125,27 @@ class Resistance:
 
 @dataclass(kw_only=True)
 class Immunity:
+    """
+    Dataclass used to track any monster immunities.
+
+    Will be part of a list, even if there is just one resistance.
+    """
+    
     type: str
     ac_modifier: float = field(default=0)
 
     def __post_init__(self):
+        """Validate that type is the correct format."""
         if not isinstance(self.type, str):
             raise InvalidTypeError("Not a valid type", self.type)
 
 @dataclass(kw_only=True)
 class Vulnerability:
+    """
+    Dataclass used to track any monster vulnerabilities.
+
+    Will be part of a list, even if there is just one resistance.
+    """
+    
     type: str
     ac_modifier: float = field(default=0)
