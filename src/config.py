549c8ed8fa3c62412251b3b760b65e34b59ba902
Tom <tom@inmail.me>
2026-02-20 12:36:07 +0100

    created reference dataclass in config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 import os
 from dotenv import load_dotenv, find_dotenv
+import polars as pl
+from pathlib import Path
 
 load_dotenv(find_dotenv())
 
@@ -23,3 +25,20 @@ def load_settings() -> Settings:
         raise RuntimeError("DB_URL not set")
 
     return Settings(api_url=api_url, api_lang=api_lang, db_url=db_url, news_category=news_category)
+
+@dataclass(frozen=True)
+class ChallengeRatingReference:
+    reference: pl.DataFrame
+
+    def get_reference_base(self) -> pl.DataFrame:
+        return self.reference.drop(columns=["dpr_legend_min", "dpr_legend_max"])
+    
+    def get_references_legend(self) -> pl.DataFrame:
+        return self.reference.select(["challenge_rating", "dpr_legend_min", "dpr_legend_max"])
+
+
+def load_challenge_rating_reference() -> ChallengeRatingReference:
+    df_filepath = Path("data/5E/baseline_stats.csv")
+    df = pl.read_csv(df_filepath)
+    return ChallengeRatingReference(reference=df)
+
