f079fc676e9b4ae43ed5465538809d0585eacc48
Tom Oliver <tom@inmail.me>
2026-07-17 14:56:39 +0200

    removed custom exceptions as the exceptions they checked were redundant

--- src/monsteriser_monster_generator/monsterizer_dataclasses/dataclass_exceptions.py
+++
@@ -1,7 +0,0 @@
-def InvalidTypeError(Exception):
-    def __init__(self, message, type):
-        super().__init__(message)
-        self.type = type
-
-    def str(self):
-        return f"{self.message} for {self.type}"
